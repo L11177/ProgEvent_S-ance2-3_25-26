50be110e2565bc604b7789f335b6f46636269c2b
L11177 <Luka.PASAOGLU@student.hel.be>
2026-03-03 08:54:38 +0100

    Séance 7 version finale (clavier/souris)

---
+++ EcranClavierSouris.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProgEvent_Séance2_25_26
+{
+    partial class EcranClavierSouris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlSouris = new Panel();
+            lblClavier = new Label();
+            lblClicGauche = new Label();
+            lblClicDroit = new Label();
+            lblPositionX = new Label();
+            lblPositionY = new Label();
+            lsbClavier = new ListBox();
+            butRAZ = new Button();
+            tbxClicGauche = new TextBox();
+            tbxClicDroit = new TextBox();
+            tbxPositionX = new TextBox();
+            tbxPositionY = new TextBox();
+            SuspendLayout();
+            // 
+            // pnlSouris
+            // 
+            pnlSouris.BackColor = Color.DimGray;
+            pnlSouris.BorderStyle = BorderStyle.Fixed3D;
+            pnlSouris.Location = new Point(230, 66);
+            pnlSouris.Name = "pnlSouris";
+            pnlSouris.Size = new Size(490, 347);
+            pnlSouris.TabIndex = 0;
+            pnlSouris.Paint += pnlSouris_Paint;
+            pnlSouris.MouseDown += pnlSouris_MouseDown;
+            pnlSouris.MouseMove += pnlSouris_MouseMove;
+            // 
+            // lblClavier
+            // 
+            lblClavier.AutoSize = true;
+            lblClavier.Location = new Point(26, 9);
+            lblClavier.Name = "lblClavier";
+            lblClavier.Size = new Size(138, 20);
+            lblClavier.TabIndex = 1;
+            lblClavier.Text = "Interception Clavier";
+            // 
+            // lblClicGauche
+            // 
+            lblClicGauche.AutoSize = true;
+            lblClicGauche.Location = new Point(230, 9);
+            lblClicGauche.Name = "lblClicGauche";
+            lblClicGauche.Size = new Size(85, 20);
+            lblClicGauche.TabIndex = 2;
+            lblClicGauche.Text = "Clic gauche";
+            // 
+            // lblClicDroit
+            // 
+            lblClicDroit.AutoSize = true;
+            lblClicDroit.Location = new Point(366, 9);
+            lblClicDroit.Name = "lblClicDroit";
+            lblClicDroit.Size = new Size(69, 20);
+            lblClicDroit.TabIndex = 3;
+            lblClicDroit.Text = "Clic droit";
+            // 
+            // lblPositionX
+            // 
+            lblPositionX.AutoSize = true;
+            lblPositionX.Location = new Point(516, 9);
+            lblPositionX.Name = "lblPositionX";
+            lblPositionX.Size = new Size(74, 20);
+            lblPositionX.TabIndex = 4;
+            lblPositionX.Text = "Position X";
+            // 
+            // lblPositionY
+            // 
+            lblPositionY.AutoSize = true;
+            lblPositionY.Location = new Point(635, 9);
+            lblPositionY.Name = "lblPositionY";
+            lblPositionY.Size = new Size(73, 20);
+            lblPositionY.TabIndex = 5;
+            lblPositionY.Text = "Position Y";
+            // 
+            // lsbClavier
+            // 
+            lsbClavier.FormattingEnabled = true;
+            lsbClavier.Location = new Point(26, 36);
+            lsbClavier.Name = "lsbClavier";
+            lsbClavier.Size = new Size(198, 344);
+            lsbClavier.TabIndex = 6;
+            lsbClavier.KeyDown += lsbClavier_KeyDown;
+            lsbClavier.KeyPress += lsbClavier_KeyPress;
+            lsbClavier.KeyUp += lsbClavier_KeyUp;
+            // 
+            // butRAZ
+            // 
+            butRAZ.BackColor = SystemColors.ActiveBorder;
+            butRAZ.Location = new Point(26, 385);
+            butRAZ.Name = "butRAZ";
+            butRAZ.Size = new Size(173, 44);
+            butRAZ.TabIndex = 7;
+            butRAZ.Text = "Remise à Zero";
+            butRAZ.UseVisualStyleBackColor = false;
+            butRAZ.Click += butRAZ_Click;
+            // 
+            // tbxClicGauche
+            // 
+            tbxClicGauche.Location = new Point(230, 36);
+            tbxClicGauche.Name = "tbxClicGauche";
+            tbxClicGauche.ReadOnly = true;
+            tbxClicGauche.Size = new Size(85, 27);
+            tbxClicGauche.TabIndex = 8;
+            // 
+            // tbxClicDroit
+            // 
+            tbxClicDroit.Location = new Point(366, 36);
+            tbxClicDroit.Name = "tbxClicDroit";
+            tbxClicDroit.ReadOnly = true;
+            tbxClicDroit.Size = new Size(85, 27);
+            tbxClicDroit.TabIndex = 9;
+            // 
+            // tbxPositionX
+            // 
+            tbxPositionX.Location = new Point(516, 36);
+            tbxPositionX.Name = "tbxPositionX";
+            tbxPositionX.ReadOnly = true;
+            tbxPositionX.Size = new Size(85, 27);
+            tbxPositionX.TabIndex = 10;
+            // 
+            // tbxPositionY
+            // 
+            tbxPositionY.Location = new Point(635, 36);
+            tbxPositionY.Name = "tbxPositionY";
+            tbxPositionY.ReadOnly = true;
+            tbxPositionY.Size = new Size(85, 27);
+            tbxPositionY.TabIndex = 9;
+            // 
+            // EcranClavierSouris
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(747, 450);
+            Controls.Add(tbxPositionY);
+            Controls.Add(tbxPositionX);
+            Controls.Add(tbxClicDroit);
+            Controls.Add(tbxClicGauche);
+            Controls.Add(butRAZ);
+            Controls.Add(lsbClavier);
+            Controls.Add(lblPositionY);
+            Controls.Add(lblPositionX);
+            Controls.Add(lblClicDroit);
+            Controls.Add(lblClicGauche);
+            Controls.Add(lblClavier);
+            Controls.Add(pnlSouris);
+            Name = "EcranClavierSouris";
+            Text = "Clavier/Souris";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlSouris;
+        private Label lblClavier;
+        private Label lblClicGauche;
+        private Label lblClicDroit;
+        private Label lblPositionX;
+        private Label lblPositionY;
+        private ListBox lsbClavier;
+        private Button butRAZ;
+        private TextBox tbxClicGauche;
+        private TextBox tbxClicDroit;
+        private TextBox tbxPositionX;
+        private TextBox tbxPositionY;
+    }
+}
---
+++ EcranClavierSouris.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgEvent_Séance2_25_26
+{
+    public partial class EcranClavierSouris : Form
+    {
+        private int nbGauche = 0;
+        private int nbDroit = 0;
+        public EcranClavierSouris()
+        {
+            InitializeComponent();
+        }
+
+        private void AfficherClic()
+        {
+            tbxClicGauche.Text = nbGauche.ToString();
+            tbxClicDroit.Text = nbDroit.ToString();
+        }
+        private void lsbClavier_KeyDown(object sender, KeyEventArgs e)
+        {
+            lsbClavier.Items.Insert(0, "Touche enfoncée : " + e.KeyCode);
+        }
+
+        private void lsbClavier_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lsbClavier.Items.Insert(0, "Caractère tapé : " + e.KeyChar);
+        }
+
+        private void lsbClavier_KeyUp(object sender, KeyEventArgs e)
+        {
+            lsbClavier.Items.Insert(0, "Touche relâchée : " + e.KeyCode);
+        }
+
+        private void pnlSouris_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                nbGauche = nbGauche + 1;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                nbDroit = nbDroit + 1;
+            }
+
+            AfficherClic(); 
+            
+            int colonne = (e.X / (pnlSouris.Width / 3)) + 1;
+            int ligne = (e.Y / (pnlSouris.Height / 3)) + 1;
+
+            if (colonne > 3) colonne = 3;
+            if (ligne > 3) ligne = 3;
+
+            lsbClavier.Items.Insert(0, "Clic case : Ligne " + ligne + ", Col " + colonne);
+        }
+
+        private void pnlSouris_MouseMove(object sender, MouseEventArgs e)
+        {
+            tbxPositionX.Text = e.X.ToString();
+            tbxPositionY.Text = e.Y.ToString();
+        }
+
+        private void butRAZ_Click(object sender, EventArgs e)
+        {
+            nbGauche = 0;
+            nbDroit = 0;
+            AfficherClic();
+
+            tbxPositionX.Text = "";
+            tbxPositionY.Text = "";
+
+            lsbClavier.Items.Clear();
+        }
+
+        private void pnlSouris_Paint(object sender, PaintEventArgs e)
+        {
+            int w = pnlSouris.Width / 3;
+            int h = pnlSouris.Height / 3;
+
+            e.Graphics.DrawLine(Pens.Black, w, 0, w, pnlSouris.Height);
+            e.Graphics.DrawLine(Pens.Black, 2 * w, 0, 2 * w, pnlSouris.Height);
+
+            e.Graphics.DrawLine(Pens.Black, 0, h, pnlSouris.Width, h);
+            e.Graphics.DrawLine(Pens.Black, 0, 2 * h, pnlSouris.Width, 2 * h);
+        }
+    }
+}
